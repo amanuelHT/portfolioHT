a142ba3e3dd576cb89821819ebffb325ba2cb787
Amanuel Tsegay <156679409+amanuelHT@users.noreply.github.com>
2026-04-26 05:36:10 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <About />
       <Education />
         <Projects />
-      <Experience />
+      {/* <Experience /> */}
       <Remarks />
     </main>
   );
